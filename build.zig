33083628098a8315c36910ec204b7cd7292ba7ca
ej-shafran <ejshafran@hilma.tech>
2024-05-23 00:38:42 +0300

    Raylib hello world

--- build.zig
+++ build.zig
@@ -1,12 +1,30 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "main",
         .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .target = target,
+        .optimize = optimize,
     });
+
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const raylib = raylib_dep.module("raylib");
+    const raylib_math = raylib_dep.module("raylib-math");
+    const rlgl = raylib_dep.module("rlgl");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raylib-math", raylib_math);
+    exe.root_module.addImport("rlgl", rlgl);
+
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const rl = @import("raylib");
 
 pub fn main() !void {
-    std.debug.print("Hello, world\n", .{});
+    rl.initWindow(800, 800, "zothello");
+    defer rl.closeWindow();
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.red);
+    }
 }
